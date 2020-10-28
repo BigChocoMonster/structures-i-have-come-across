e6bc536cc4f2d8c7e9a116b0e9c44f4ae1a8078c
Deblina Pattanayak <bigchocomonster@gmail.com>
2020-10-28 20:48:47 +0530

    - fixed route change detection

--- src/components/Bar/index.tsx
+++ src/components/Bar/index.tsx
@@ -5,6 +5,7 @@ import "./styles.scss";
 export default class Bar extends Component<{}, {}> {
   setRoute(route: string) {
     window.history.pushState(null, route, `/${route.toLowerCase()}`);
+    window.dispatchEvent(new Event("popstate"));
   }
 
   render() {
